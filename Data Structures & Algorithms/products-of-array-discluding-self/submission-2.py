a741de90ba0a4cc1fbe390b783463fe0f5adca82
Muhammad Saad Babar <109409707+saadbabar@users.noreply.github.com>
2026-08-09 11:12:17 -0400

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # first and last number index, multiply by one
+        prefix = [0] * len(nums)
+        postfix = [0] * len(nums)
+        output = [0] * len(nums)
+
+        for i in range(len(nums)):
+            if i == 0:
+                prefix[i] = 1
+            else:
+                prefix[i] = prefix[i - 1] * nums[i - 1]
+
+        for i in reversed(range(len(nums))):
+            if i == len(nums) - 1:
+                postfix[i] = 1
+            else:
+                postfix[i] = postfix[i + 1] * nums[i + 1]
+
+        # 1 1 2 8
+        # 48 24 6 1
+        for i in range(len(nums)):
+            output[i] = postfix[i] * prefix[i]
+
+        return output
